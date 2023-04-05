45aa41b562b1f93e2ab3d6ec45c1451d01a16c5e
Victoria Mertens <victoria.mertens44@gmail.com>
2023-04-05 15:27:19 -0500

    Reconfigured package json for Modules and updated server.cjs to match.

---
+++ server/server.cjs
@@ -0,0 +1,15 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+
+const app = express();
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
