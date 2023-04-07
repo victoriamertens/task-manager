4296471270e4dfa9fa2d16824006cf143e577b89
Victoria Mertens <victoria.mertens44@gmail.com>
2023-04-07 09:48:46 -0500

    Fixed SQL query bug

--- database.sql
+++ database.sql
@@ -1,30 +1,32 @@
 /* Database name in PostgreSQL = "task-manager-data" */ 
 
+CREATE TABLE categories ( 
+  "id" serial PRIMARY KEY,
+  "name" VARCHAR(40), 
+  "description" VARCHAR(150)
+);
+
 CREATE TABLE tasks ( 
   "id" serial PRIMARY KEY, 
   "name" VARCHAR(80), 
   "description" VARCHAR(150), 
   "completed" boolean default false, 
-  "category_id" FOREIGN KEY REFERENCES categories(id)
-)
+  "category_id" INT REFERENCES categories(id)
+);
 
 CREATE TABLE sub_tasks ( 
   "id" serial PRIMARY KEY, 
-  "task_id" FOREIGN KEY REFERENCES tasks(id), 
+  "task_id" INT FOREIGN KEY REFERENCES tasks(id), 
   "name" VARCHAR(80), 
   "description" VARCHAR(150), 
   "completed" boolean DEFAULT false
-)
+);
 
-CREATE TABLE categories ( 
-  "id" serial PRIMARY KEY,
-  "name" VARCHAR(40), 
-  "description" VARCHAR(150)
-)
+
 
 CREATE TABLE work_times ( 
   "id" serial PRIMARY KEY,
   "date" DATE, 
   "start-time" TIME, 
   "end-time" TIME
-)
+);
